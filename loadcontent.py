09982a9bbc8092e71dbfa9e5dfe463ecd96a8bcf
Scott Boone <scott.e.boone@gmail.com>
2018-10-16 00:38:54 -0500

    added unique ids for memes and changed hash to deal with that

--- loadcontent.py
+++ loadcontent.py
@@ -10,17 +10,19 @@ with open('content.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         # all elements are filled in and ready is marked
-        if all(v for v in row[:5]) and (row[5] == 'yes' or row[5] == 'y'):  
-            memes.append((row[0] + '.jpg', row[1], row[2], row[3], row[4]))
+        if all(v for v in row[:6]) and (row[6] == 'yes' or row[6] == 'y'):  
+            memes.append((row[0], row[1] + '.jpg', row[2], row[3], row[4], row[5]))
 
 with open('static/memes.js', 'w') as outfile:
-    outfile.write('''var excuse = [];
+    outfile.write('''var hashes = [];
+var excuse = [];
 var meme = [];
 var alt = [];
 var footnotes = [];
 var info = [];
 
 // loading page
+hashes.push("");
 meme.push("eagle.jpg");
 alt.push("What is your excuse not to vote?");
 excuse.push("");
@@ -28,11 +30,12 @@ footnotes.push("");
 info.push("");''')
     for m in memes[1:]:  # skip header
         outfile.write('''
+hashes.push("{}");
 meme.push("{}");
 alt.push("{}");
 excuse.push("{}");
 footnotes.push("{}");
-info.push("{}");'''.format(*mesc))
+info.push("{}");'''.format(*m))
     
     outfile.close()
 
