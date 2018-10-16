0e4f760019457a782869bdcf6d3cb4f484e64079
Scott Boone <scott.e.boone@gmail.com>
2018-10-15 23:17:13 -0500

    restructured. loaded new content, completed load script

--- loadcontent.py
+++ loadcontent.py
@@ -3,18 +3,37 @@
 import csv
 import json
 
-out = []
+# BUG: does not deal nicely with " characters
+
+memes = []
 with open('content.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
-        if all(v for v in row[:5]):  # all elements are filled in
-            out.append({'image': row[0] + '.jpg', 'alttext': row[1], 
-                'excuse': row[2], 'answer': row[3], 'source': row[4]})
+        # all elements are filled in and ready is marked
+        if all(v for v in row[:5]) and (row[5] == 'yes' or row[5] == 'y'):  
+            memes.append((row[0] + '.jpg', row[1], row[2], row[3], row[4]))
 
-with open('memes.json', 'w') as outfile:
-    s = json.dumps(out[1:]) # dump all but the header row
-    s = s.replace("'", "\\'")
-    outfile.write(s)
+with open('static/memes.js', 'w') as outfile:
+    outfile.write('''var excuse = [];
+var meme = [];
+var alt = [];
+var footnotes = [];
+var info = [];
+
+// loading page
+meme.push("eagle.jpg");
+alt.push("What is your excuse not to vote?");
+excuse.push("");
+footnotes.push("");
+info.push("");''')
+    for m in memes[1:]:  # skip header
+        outfile.write('''
+meme.push("{}");
+alt.push("{}");
+excuse.push("{}");
+footnotes.push("{}");
+info.push("{}");'''.format(*mesc))
+    
     outfile.close()
 
 
